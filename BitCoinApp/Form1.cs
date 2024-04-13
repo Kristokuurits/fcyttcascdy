ef9d81146db309143666a617955a1378afd9bbf4
Kristo Kuurits <Kristo.kuurits@gmail.com>
2024-04-13 23:20:38 +0300

    a
    a

--- BitCoinApp/Form1.cs
+++ BitCoinApp/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,6 +9,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace BitCoinApp
 {
@@ -22,25 +22,46 @@ namespace BitCoinApp
 
         private void btnGetRates_Click(object sender, EventArgs e)
         {
+            if (CurrencyCombo.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a currency.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(amountOfCoinBox.Text))
+            {
+                MessageBox.Show("Please enter a BTC amount.");
+                return;
+            }
+
+            resultLabel.Visible = true;
+            ResultTextBox.Visible = true;
+            BitcoinRates bitcoin = GetRates();
+
+            float rate;
+            string code;
+
             if (CurrencyCombo.SelectedItem.ToString() == "EUR")
             {
-                resultLabel.Visible = true;
-                ResultTextBox.Visible = true;
-                BitCoinRates bitcoin = GetRates();
-                float result = Int32.Parse(amountOfCoinBox.Text) * bitcoin.bpi.EUR.rate_float;
-                ResultTextBox.Text = $"{result.ToString()} {bitcoin.bpi.EUR.code}";
+                rate = bitcoin.bpi.EUR.rate_float;
+                code = bitcoin.bpi.EUR.code;
             }
-            if (CurrencyCombo.SelectedItem.ToString() == "USD")
+            else if (CurrencyCombo.SelectedItem.ToString() == "USD")
             {
-                resultLabel.Visible = true;
-                ResultTextBox.Visible = true;
-                BitCoinRates bitcoin = GetRates();
-                float result = Int32.Parse(amountOfCoinBox.Text) * bitcoin.bpi.USD.rate_float;
-                ResultTextBox.Text = $"{result.ToString()} {bitcoin.bpi.USD.code}";
+                rate = bitcoin.bpi.USD.rate_float;
+                code = bitcoin.bpi.USD.code;
             }
+            else
+            {
+                MessageBox.Show("Unsupported currency.");
+                return;
+            }
+
+            float result = float.Parse(amountOfCoinBox.Text) * rate;
+            ResultTextBox.Text = $"{result.ToString()} {code}";
         }
 
-        public static BitCoinRates GetRates()
+        public static BitcoinRates GetRates()
         {
             string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
@@ -49,14 +70,56 @@ namespace BitCoinApp
             var webResponse = request.GetResponse();
             var webStream = webResponse.GetResponseStream();
 
-            BitCoinRates bitcoin;
-            using(var responseReader = new StreamReader(webStream))
+            BitcoinRates bitcoin;
+            using (var responseReader = new StreamReader(webStream))
             {
                 var response = responseReader.ReadToEnd();
-                bitcoin = JsonConvert.DeserializeObject<BitCoinRates>(response);
+                bitcoin = JsonConvert.DeserializeObject<BitcoinRates>(response);
             }
 
             return bitcoin;
         }
+    }
+
+    public class BitcoinRates
+    {
+        public Bpi bpi { get; set; }
+    }
+
+    public class Bpi
+    {
+        public Eur EUR { get; set; }
+        public Usd USD { get; set; }
+        public Gbp GBP { get; set; }
+    }
+
+    public class Eur
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public float rate_float { get; set; }
+        public string rate_string { get; set; }
+        public string description { get; set; }
+        public string rate_html { get; set; }
+    }
+
+    public class Usd
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public float rate_float { get; set; }
+        public string rate_string { get; set; }
+        public string description { get; set; }
+        public string rate_html { get; set; }
+    }
+
+    public class Gbp
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public float rate_float { get; set; }
+        public string rate_string { get; set; }
+        public string description { get; set; }
+        public string rate_html { get; set; }
     }
 }
